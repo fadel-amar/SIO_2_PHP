03ddced09d24202106546115835a335a9ab816ca
fadel-amar <fadel-amar@outlook.com>
2023-09-12 11:55:47 +0200

    Commencement, Création controller Bienenue

---
+++ src/Controller/BienvenueController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BienvenueController extends AbstractController
+{
+    #[Route('/bienvenue', name: 'app_bienvenue')]
+    public function bienvenue(): Response
+    {
+        // Appel à la vue
+        return $this->render('bienvenue/index.html.twig');
+    }
+}
