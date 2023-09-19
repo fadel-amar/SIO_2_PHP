966bfc6faeab8b4581c237ee0dff90976e13d084
fadel-amar <fadel-amar@outlook.com>
2023-09-19 10:38:54 +0200

    Mise en place fixtures

--- src/DataFixtures/EtudiantFixtures.php
+++ src/DataFixtures/EtudiantFixtures.php
@@ -5,15 +5,25 @@ namespace App\DataFixtures;
 use App\Entity\Etudiant;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class EtudiantFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        for ($i = 0; $i < 10; $i++) {
+        // Créer un objet de type faker
+        $faker = Factory::create("fr_FR");
+        // Générer 100 étudiants
+        for ($i = 1 ; $i <100 ; $i++) {
             // Créer un étudiant
-            $etudiant = new Etudiant();
-            $etudiant->set("papaPoule")
+            $etudiant = new  Etudiant();
+            $etudiant->setPrenom($faker->firstName);
+            $etudiant->setNom($faker->lastName);
+            $etudiant->setEmail($faker->email);
+            $etudiant->setDateNaissance($faker->dateTimeBetween('-28 years', '-17 years'));
+            // Persister l'étudiant
+            // Doctrine va générer un INSERT INTO
+            $manager->persist($etudiant);
         }
 
         $manager->flush();
