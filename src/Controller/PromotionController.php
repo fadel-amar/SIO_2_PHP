d0c16cebf6b4769ac4fd0dfffe141a706acf68bc
fadel-amar <fadel-amar@outlook.com>
2023-09-20 09:51:40 +0200

    Réalisation exercice Promotions affichage de la vue

--- src/Controller/PromotionController.php
+++ src/Controller/PromotionController.php
@@ -2,17 +2,40 @@
 
 namespace App\Controller;
 
+use App\Repository\EtudiantRepository;
+use App\Repository\PromotionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PromotionController extends AbstractController
 {
-    #[Route('/promotion', name: 'app_promotion')]
-    public function index(): Response
+    #[Route('/promotions', name: 'app_promotion_liste')]
+    public function list( PromotionRepository $promotionRepository): Response
     {
+        $promotions = $promotionRepository->findAll();
+
         return $this->render('promotion/index.html.twig', [
-            'controller_name' => 'PromotionController',
+            'promotions' => $promotions,
         ]);
     }
+
+    #[Route('/promotions/{id}', name: 'app_promotion_info', requirements: ['id' => '\d+'])]
+    public function promotion( PromotionRepository $promotionRepository, EtudiantRepository $etudiantRepository ,int $id): Response
+    {
+        // Appel au modèle
+        $promotion = $promotionRepository->find($id);
+        /*$listeEtudiants =  $etudiantRepository->findBy(array(   = $id));*/
+
+        // Appel à la vue
+        return $this->render('promotion/promotion.html.twig',[
+            "promotion" => $promotion,
+        ]);
+    }
+
+
+
+
+
+
 }
